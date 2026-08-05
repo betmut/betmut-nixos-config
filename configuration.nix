4538ebf438c099b4ca10aef5aba68512a3875567
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-08-05 14:27:55 +0700

    Refactor configuration files

--- configuration.nix
+++ configuration.nix
@@ -1,43 +1,18 @@
 {config, pkgs, inputs, modulesPath, ... }: {
+  
+  system.stateVersion = "26.05";
 
   _module.args.pkgs-stable = import inputs.nixpkgs-stable {
     system = "x86_64-linux";
     config.allowUnfree = true;
   };
 
-  system.stateVersion = "26.05";
   imports = [
     #common modules
     ./modules/common.nix
     ./stylix.nix
     ./disks.nix
+    ./nix-settings.nix
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
-  
-  nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      substituters = [
-        "https://hyprland.cachix.org"
-        "https://nix-community.cachix.org"
-      ];
-      trusted-substituters = [
-        "https://hyprland.cachix.org"
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
-    optimise = {
-      automatic = true;
-      dates = [ "20:00" ];
-    };
-  };
 }
