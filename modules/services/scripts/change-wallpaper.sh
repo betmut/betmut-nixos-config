2db62794db2aeebff11c99eff22988ee4c9dab1a
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-08-09 00:28:24 +0700

    change emoji in change-wallpaper.sh script

--- modules/services/scripts/change-wallpaper.sh
+++ modules/services/scripts/change-wallpaper.sh
@@ -30,8 +30,8 @@ fi
 # 10# forces Bash to treat the numbers as base-10 to prevent octal errors on '08' or '09'
 if (( 10#$CURRENT_TIME >= 600 && 10#$CURRENT_TIME < 1800 )); then
     waypaper --random --monitor All --backend awww --folder "$LIGHT_DIR" | grep "Sent" > "$MSG"
-    notify-send -i "$LIGHT_DIR/$(cat $MSG | awk '{print $6}')" "✨ Day Wallpaper Updated" "$(cat $MSG)"
+    notify-send -i "$LIGHT_DIR/$(cat $MSG | awk '{print $6}')" "☀️ Day Wallpaper Updated" "$(cat $MSG)"
 else
     waypaper --random --monitor All --backend awww --folder "$DARK_DIR" | grep "Sent" > "$MSG"
-    notify-send -i "$DARK_DIR/$(cat $MSG | awk '{print $6}')" "✨ Night Wallpaper Updated" "$(cat $MSG)"
+    notify-send -i "$DARK_DIR/$(cat $MSG | awk '{print $6}')" "🌙 Night Wallpaper Updated" "$(cat $MSG)"
 fi
