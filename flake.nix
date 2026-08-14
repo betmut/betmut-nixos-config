f7ab557c10f6531a9c7f24d57257cc40858e5560
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-08-14 19:21:42 +0700

    Update nix-darwin version to 26.05 in flake.nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     };
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
