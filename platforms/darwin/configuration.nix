bd5ebb785fb3ab80f371914b2490114cb8f0b71a
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-08-15 09:50:16 +0700

    Update system.stateVersion to 7 in darwin configuration

--- platforms/darwin/configuration.nix
+++ platforms/darwin/configuration.nix
@@ -1,6 +1,6 @@
 {lib, config, pkgs, inputs, ... }: {
 
-  system.stateVersion = "26.05";
+  system.stateVersion = 7;
 
   _module.args.pkgs-stable = import inputs.nixpkgs-stable {
     system = "x86_64-linux";
