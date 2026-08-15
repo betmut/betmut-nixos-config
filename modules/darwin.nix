80f2533f99adff75376acd45006d472555c56d4d
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-08-15 08:19:18 +0700

    Fix darwin build command to target system attribute; format homebrew configuration

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -1,21 +1,21 @@
 {config, pkgs, inputs, lib, ... }: {
-    homebrew = {
-        enable = true;
-        casks = [
-            "racket"
-            "qutebrowser"
-            "obsidian"
-            "whatsapp"
-            "gimp"
-            "inkscape"
-            "figma"
-            "minecraft"
-            "spotify"
-            "capcut"
-            "discord"
-            "vlc"
-            "warzone-2100"
-            "virtualbox"
-        ];
-    };   
+  homebrew = {
+    enable = true;
+    casks = [
+      "racket"
+      "qutebrowser"
+      "obsidian"
+      "whatsapp"
+      "gimp"
+      "inkscape"
+      "figma"
+      "minecraft"
+      "spotify"
+      "capcut"
+      "discord"
+      "vlc"
+      "warzone-2100"
+      "virtualbox"
+    ];
+  };   
 }
