f62c7bdc1dbcca28617f9ed877c76613812d1664
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-08-15 11:29:17 +0700

    Update home-manager-darwin-stable URL to point to release-26.05

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
     };
 
     home-manager-darwin-stable = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs-stable-darwin";
     };
 
