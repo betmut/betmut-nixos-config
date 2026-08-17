cfe0b3ee914294c2afb1f8217488a082306aa2e1
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-08-18 06:10:32 +0700

    raww

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
-  boot.loader.efiInstallAsRemovable = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.efi.canTouchEfiVariables = false;
 
   nixpkgs.config = {
